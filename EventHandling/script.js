ca79d6c9be200d949e64c29bb7183d1fde6654e7
Yash Malvade <yashmalvade91@gmail.com>
2025-02-20 20:33:59 +0530

    Add files via upload

--- EventHandling/script.js
+++ EventHandling/script.js
@@ -1,48 +1,13 @@
-let s1 = document.getElementById("sq1");
-s1.addEventListener("mouseenter", function() {
-    let r = Math.floor(Math.random() * 100);
-    s1.innerHTML = `<h1>${r}</h1>`;
-});
-s1.addEventListener("mouseleave", function() {
-    s1.innerHTML = "<h1>1</h1>";
-});
+let modeBtn = document.querySelector("#mode");
+let currMode = "light";
 
-let s2 = document.getElementById("sq2");
-let clr = "green";
-s2.addEventListener("mouseenter", function() {
-    if (clr == "green") {
-        s2.style.backgroundColor = "green";
-        clr = "red";
+modeBtn.addEventListener("click", () => {
+    if (currMode === "light") {  // ✅ Use string literals
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor="black";
     } else {
-        s2.style.backgroundColor = "red";
-        clr = "green";
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor="white";
     }
+    console.log(currMode);
 });
-s2.addEventListener("mouseleave", function() {
-    s2.style.backgroundColor = "white";
-});
-let s3 = document.getElementById("sq3");
-s3.addEventListener("mouseenter", function() {
-    let r1 = Math.floor(Math.random() * 256);
-    let r2 = Math.floor(Math.random() * 256);
-    let r3 = Math.floor(Math.random() * 256);
-    s3.style.backgroundColor=`rgb(${r1},${r2},${r3})`;
-});
-s3.addEventListener("mouseleave", function() {
-  s3.style.backgroundColor="white";
-});
-let s4 = document.getElementById("sq4");
-s4.addEventListener("click", function() {
-    let r1 = Math.floor(Math.random() * 256);
-    let r2 = Math.floor(Math.random() * 256);
-    let r3 = Math.floor(Math.random() * 256);
-    s1.style.backgroundColor=`rgb(${r1},255,255)`;
-    s2.style.backgroundColor=`rgb(255,${r2},255)`;
-    s3.style.backgroundColor=`rgb(255,255,${r3})`;
-});
-s4.addEventListener("mouseleave", function() {
-    s1.style.backgroundColor = "white";
-    s2.style.backgroundColor = "white";
-    s3.style.backgroundColor = "white";
-});
-
