588057e58f096d0087c20c4d2bbe6919867b945d
Yash Malvade <yashmalvade91@gmail.com>
2025-02-21 21:39:32 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,22 @@
+// // Arrays in Js
+// const Words=['Apple','Bannna','Cherry',12,true];
+// console.log(Words.length);
+// // Arrays Methods
+const nums=[9,5,3,2,7,10,5,4];
+// console.log(nums.indexOf(9));
+// console.log(nums.includes(90));
+// console.log(nums.push(90));
+// console.log(nums);
+// console.log(nums.pop(90));
+// console.log(nums);
+// console.log(nums.unshift(90));
+// console.log(nums);
+// console.log(nums.shift(90));
+// console.log(nums);
+// nums.sort();
+// console.log(nums);
+
+// slice method
+console.log(nums);
+const subArray=nums.slice(0,2);
+console.log(subArray);
