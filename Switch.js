36cb62f3797852d8445218791e09dab312e5f967
Yash Malvade <yashmalvade91@gmail.com>
2025-02-21 20:37:24 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,18 @@
+const grade = 'A';
+switch(grade){
+    case 'A':
+        console.log("Excellent");
+        break;
+        case 'B':
+        console.log("Very Good");
+        break;
+        case 'C':
+        console.log("need improvement");
+        break;
+        case 'D':
+        console.log("Fail");
+        break;
+        default:
+            console.log("Invalid Grade");
+
+}
