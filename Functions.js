f84a5bd2bc04f8ad47c961b150fda77054c77d58
Yash Malvade <yashmalvade91@gmail.com>
2025-02-22 16:10:01 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,29 @@
+// // // Callback function or // Arrow Function
+// // const greet = (count) => {
+// //     for(let i=0;i<count;i++)
+// //     console.log("Hello"); 
+// // };
+// // greet(3);
+
+// // const val = (num) => {
+// //     return num*num;
+// // }
+
+// // console.log(val(3));
+
+// // callBack Function
+
+// const calculate = (a,b,operation) => {
+//      return operation(a,b);
+// }
+// const addition = calculate(3,4 ,function(num1,num2){
+//     return num1+num2;
+// });
+// console.log(addition);
+
+// function mul(a,b){
+//     return a*b;
+// }
+// const mulresult=calculate(3,4,mul);
+//   console.log(mulresult);
+
