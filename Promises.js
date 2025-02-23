ac8961366436e63cbbec934f4ef1f44d5d3f3299
Yash Malvade <yashmalvade91@gmail.com>
2025-02-23 13:40:20 +0530

    Add files via upload

---
+++ Promises.js
@@ -0,0 +1,42 @@
+function getCheese() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const cheese = '🧀';
+            resolve(cheese);
+        }, 2000);
+    });
+}
+
+function makeDough(cheese) { // Accept cheese as a parameter
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dough = cheese + '🍩';
+            resolve(dough);
+        }, 2000);
+    });
+}
+
+function bakePizza(dough) { // Accept dough as a parameter
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const pizza = dough + '🍕';
+            resolve(pizza);
+        }, 2000);
+    });
+}
+
+getCheese()
+    .then((cheese) => {
+        console.log("Here is cheese:", cheese);
+        return makeDough(cheese);
+    })
+    .then((dough) => {
+        console.log("Here is dough:", dough);
+        return bakePizza(dough);
+    })
+    .then((pizza) => {
+        console.log("Here is pizza:", pizza);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
