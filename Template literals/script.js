c19259c5b946d299f5cfed45845c4ae30f5bd592
Yash Malvade <yashmalvade91@gmail.com>
2025-03-16 01:20:52 +0530

    Create script.js

---
+++ Template literals/script.js
@@ -0,0 +1,8 @@
+// Template literals are use with (``)
+// If we have to use variable use ${}
+// Template Literals come in ECMA Script
+let a =10;
+let b=20;
+// console.log("Sum of "+a+" And "+b+" Is "+(a+b));
+console.log(`Sum Of ${a} & ${b} Is ${a+b} `);
+
