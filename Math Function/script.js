cd606b1ddad9715fc709eae473c8089fe3fcc3f5
Yash Malvade <yashmalvade91@gmail.com>
2025-03-16 01:51:43 +0530

    Create script.js

---
+++ Math Function/script.js
@@ -0,0 +1,32 @@
+// Math Function In Javascript
+// console.log(Math.PI);
+
+// console.log(Math.round(10.499));
+
+// Math.abs() // Returns positive value
+// let a=2;
+// let b=4;
+// let c=a-b;
+// console.log(Math.abs(c));
+
+// Math.ceil() // returns  Top Value
+// console.log(Math.ceil(4.55));
+
+// Math.floor() returns bottom value
+// console.log(Math.floor(4.33));
+
+
+// Math.min() It finds minimum value
+// let arr=[2,5,6,8,3]
+// console.log(Math.min(...arr));
+
+// Math.max() It finds maximum value
+// console.log(Math.max(2,5,6,8));
+
+// // Math.random()
+// console.log(Math.round());
+// It gives random value b/w 0 to 9
+
+// Math.trunc // It gives Integer without point 
+// console.log(Math.trunc(4.55));
+
