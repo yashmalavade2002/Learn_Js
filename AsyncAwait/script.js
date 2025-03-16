94c2449951c2f838163afeacacc69aeb88ae5dff
Yash Malvade <yashmalvade91@gmail.com>
2025-03-16 16:48:16 +0530

    Update script.js

--- AsyncAwait/script.js
+++ AsyncAwait/script.js
@@ -16,3 +16,23 @@ let pepperoni = await orderPizza("Pepperoni");  // Waits for another 3 seconds
 console.log(pepperoni);
 }
 orderMultiplePizzas();
+
+
+// Async Await
+let pro = new Promise(function(resolve,reject){
+    setTimeout(function(){
+     let API = true;
+     if(API){
+        resolve("Promise Fullfilled");
+     }
+     else{
+        reject("Promise rejected");
+     }
+    },4000)
+})
+ async function check(){
+    let data = await pro;
+    console.log(data);
+    
+ }
+ check();
