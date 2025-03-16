1f038bac34a9431f2bc7c903634f1832a20130ad
Yash Malvade <yashmalvade91@gmail.com>
2025-03-16 17:10:46 +0530

    Create script.js

---
+++ Let  Var & Const/script.js
@@ -0,0 +1,45 @@
+// Let Var  & Const
+// var are in global scope
+//  var can be update & re declare
+// Let can be update but cannot be re-declare
+// const cannot be update and re-declare
+
+// ES6 ECMA script :
+// Always use let and const dont use const
+
+// var c= 30;
+// let d = 60;
+  
+// {
+//     let a=10;
+//     const b=20;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c)
+    
+    
+// }
+
+// var
+ var a=20;
+ a=60; // update
+ var a=40; // re de-clare
+ console.log(a);
+ 
+
+// let
+// let a=20;
+// a=40;
+// let a =50;
+// console.log(a);
+
+
+// const
+// const c=100;
+//  c=30;
+//  const c=120;
+// console.log(c);
+
+
+
+
