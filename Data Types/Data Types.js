dd0ddcff89703ed39ec3ddcfd1fe5e46ad5b4dbf
Yash Malvade <yashmalvade91@gmail.com>
2025-03-30 19:47:39 +0530

    Create Data Types.js

---
+++ Data Types/Data Types.js
@@ -0,0 +1,59 @@
+Data Types
+primitive data types
+Number
+String
+Boolean
+undefined
+null
+BigInt
+Symbol
+
+Number
+let num1= 34;
+let  num2=34.6;
+console.log(typeof num1, num1);
+
+// Infinity
+// NaN
+let  num3=10/0;
+console.log(num3);
+
+let num4='kishan'/2;
+console.log(num4);
+
+
+string
+let myName ='kishan yadav'
+console.log(typeof myName);
+
+
+boolean
+let isloggin =false;
+console.log(typeof isloggin ,isloggin);
+
+
+undefined
+let a;
+console.log(a);
+
+
+
+symbol
+let myName = Symbol('kishan');
+let myName2 = 'kishan';
+console.log(myName == myName2);
+
+bigint
+let num1 = BigInt(2356789);
+console.log(typeof num1);
+
+
+Non primitive Arrays objects functions
+
+obj
+let obj = {
+    name : 'yash',
+    course : 'JS'
+}
+console.log(obj);
+
