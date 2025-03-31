8686d442ddc6701a5cc0544fda747efe1d23a032
Yash Malvade <yashmalvade91@gmail.com>
2025-03-31 13:57:13 +0530

    Create script.js

---
+++ Strings/script.js
@@ -0,0 +1,31 @@
+// Strings
+// A JS String is zero or more character written inside quotes
+let  str='Hello';
+// console.log(  str[2]);
+console.log(typeof str);
+
+
+
+// Index
+// code
+// 0123
+
+// length
+// we can find the length of string
+// search => indexOf
+
+let str1 ='code';
+// console.log(str1.length);
+console.log(str1.search('d'));
+
+// IndexOf :
+
+// we can find the index of any string
+
+// let str1 ='code';
+// // console.log(str1.length);
+// console.log(str1.indexOf('e'));
+
+
+
+
