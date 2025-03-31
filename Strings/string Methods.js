42cb2c170c5cd0447290d5ed031191860d220d38
Yash Malvade <yashmalvade91@gmail.com>
2025-03-31 16:59:48 +0530

    Create string Methods.js

---
+++ Strings/string Methods.js
@@ -0,0 +1,68 @@
+// Strings Methods
+
+// slice
+// let str  ="Hello All";
+// let add = str.slice(0,7);
+// console.log(add);
+
+// substring
+// let str  ="Hello All";
+// let add = str.substring(0,7);
+// console.log(add);
+
+
+// subStr
+// let str  ="Hello All";
+// let add = str.substr(-3);
+// console.log(add);
+
+
+// replace
+// let str  ="Hello All";
+// let add = str.replace("All","Buddy");
+// console.log(add);
+
+
+// chatAt
+// let str  ="Hello All";
+// let add = str.charAt(0);
+// console.log(add);
+
+// Trim
+// let str  ="    Hello All";
+// let add = str.trim();
+// console.log(str);
+// console.log(add);
+
+// split
+//  convert string into array
+// let str  ="Hello All";
+// let add = str.split();
+// console.log(add);
+
+
+// uppercase
+// let str  ="code";
+// let add = str.toUpperCase(str);
+// console.log(add);
+
+
+
+
+// lowercase
+// let str  ="Hello All";
+// let add = str.toLowerCase(str);
+// console.log(add);
+
+
+// concat 
+// join two or more strings
+// let str  ="Hello All ";
+// let fr =" Namaste"
+// let add = str.concat(fr);
+// console.log(add);
+
+
+
+
+
